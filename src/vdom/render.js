b9f21571125575f70d8e9f162103b5272ac075d6
Matthias <matvp91@gmail.com>
2019-04-13 18:38:58 +0200

    Added comments.

--- src/vdom/render.js
+++ src/vdom/render.js
@@ -2,19 +2,23 @@ import compose from './compose';
 import diff from './diff';
 import renderNode from './renderNode';
 
-const prevTreeSymbol = Symbol('vdomPrevTree');
+const SYMBOL_PREV_TREE = Symbol();
 
 export default function render(vnode, rootEl) {
+  // Create the tree, function components -> proper layout.
   const tree = compose(vnode);
-  const prevTree = rootEl[prevTreeSymbol];
-  rootEl[prevTreeSymbol] = tree;
 
-  const domStructure = renderNode(tree);
+  const prevTree = rootEl[SYMBOL_PREV_TREE];
+  rootEl[SYMBOL_PREV_TREE] = tree;
 
+  // If we don't have a previous vdom tree yet, it's the first render,
+  // simply add the nodes to the DOM instead.
   if (!prevTree) {
-    rootEl.appendChild(domStructure);
+    rootEl.appendChild(renderNode(tree));
     return;
   }
 
+  // We've got a previous vdom tree, let's diff it instead.
+  // Diffing will apply the minimal amount of patches in order to update the DOM.
   diff(rootEl.firstChild, tree, prevTree);
 }
